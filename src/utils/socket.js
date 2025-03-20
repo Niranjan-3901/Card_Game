2d9229735ed4f59801133a317208765906ac7141
Niranjan-3901 <niranjankumarbarai456@gmail.com>
2025-03-20 21:44:03 +0530

    Render Backend URL added

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client';
 
 const localhost = "http://localhost:3001";
 const ngrok = 'https://f2fa-93-190-142-118.ngrok-free.app'; // ngrok URL for backend
-export const socket = io(localhost, {
+const renderURL = "https://card-game-server-meml.onrender.com";
+export const socket = io(renderURL, {
   autoConnect: true,
   transports: ['websocket']
 });
