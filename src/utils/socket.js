372c57265e93423a9b5239f8eb2ea5adcc790d53
Niranjan-3901 <niranjankumarbarai456@gmail.com>
2025-03-20 21:54:41 +0530

    Env File add

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
-const localhost = "http://localhost:3001";
-const ngrok = 'https://f2fa-93-190-142-118.ngrok-free.app'; // ngrok URL for backend
-const renderURL = "https://card-game-server-meml.onrender.com";
-export const socket = io(renderURL, {
+// Use import.meta.env to access environment variables
+const socket = io(import.meta.env.VITE_RENDER_URL, {
   autoConnect: true,
   transports: ['websocket']
 });
+
+export { socket };
 
